d9f675922abcb44171e52596af9f1a7e8b1531fe
wastedpotential <andy@wastedpotential.com>
2016-03-11 10:55:52 -0500

    add navigation controller and save button

--- AutolayoutExample/AutolayoutExample/ViewController.swift
+++ AutolayoutExample/AutolayoutExample/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
+// MARK: - UIBarButton Actions
+    
+    @IBAction func saveBarButtonPressed(sender: UIBarButtonItem) {
+        //do something
+    }
 
 }
 
